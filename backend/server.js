52ef7fae277fcd80320a6c83e464f4798959953e
Yash Manek <yashmanek2001@gmail.com>
2025-03-23 17:42:32 +0530

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+// backend/server.js
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+
+  socket.on("canvas-update", (data) => {
+    socket.broadcast.emit("canvas-update", data); // Broadcasts elements array
+    console.log("Canvas update broadcasted:", data);
+  });
+
+  socket.on("disconnect", () => console.log("User disconnected:", socket.id));
+});
+
+server.listen(4000, () => console.log("Server running on port 4000"));
