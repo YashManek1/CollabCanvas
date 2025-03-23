a7bd4d1b6fc74af2760241dee829c3bc7dd9ddc6
Yash Manek <yashmanek2001@gmail.com>
2025-03-23 18:13:52 +0530

    Integration1

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "https://collab-canvas-liart.vercel.app/",
+    origin: "*",
     methods: ["GET", "POST"],
   },
 });
